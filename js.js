ac41b6d4b7440700dd7d9d2842acb46ec1b18074
MohiVs <mohisls2012@gmail.com>
2024-04-17 12:45:43 +0330

    add html2pdf library

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
+// import * as fs from "fs";
+
+const result = document.querySelector(".result");
+
 document.querySelector("#myForm").addEventListener("submit", (e) => {
   e.preventDefault();
   //   const formData = new FormData(e.target);
   const id = document.querySelector("#id").value;
-  const result = document.querySelector(".result");
   console.log(id);
 
   const getdata = async function () {
@@ -12,8 +15,11 @@ document.querySelector("#myForm").addEventListener("submit", (e) => {
       let user = data.find((el) => el.user_id === id);
       console.log(user);
       if (user) {
-        return (result.textContent = `${user.name} ${user.family_name} خوش آمدی`);
-      } else result.textContent = `کاربری با این مشخصات یافت نشد`;
+        const xx = `${user.name} ${user.family_name} خوش آمدی`;
+
+        html2pdf(xx);
+      }
+      result.textContent = `کاربری با این مشخصات یافت نشد`;
       if (!res.ok) throw new Error(`${data.message}${res.status}`);
     } catch (error) {
       console.log(error);
@@ -22,3 +28,6 @@ document.querySelector("#myForm").addEventListener("submit", (e) => {
 
   getdata();
 });
+
+// var element = document.getElementById("element-to-print");
+// html2pdf(element);
